877d4e2db405672e5215ab1ac774df4af6981541
ftec <felipecorreatelles@gmail.com>
2019-11-10 14:17:43 -0300

    part 2 Preventing DOS Attacks

--- server/index.js
+++ server/index.js
@@ -10,12 +10,25 @@ const port = process.env.PORT || 3001;
 
 app.use(logger('dev'));
 app.use(cors());
-app.use(express.json({ limit: '100kb'}))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/players", playersRouter);
+// Preventing DOS Attacks
+// limit the body payload using body-parser
+app.use(express.json({ limit: '100kb'})) // Body limit is 100
 
+// express feature -> express-rate-limit dependency
+// npm install express-rate-limit --save
+// setting maximum amount of requests
+// after user uses all of his requests,
+// lock him out for certain amount of time
+const limit = rateLimit({
+    max: 100,// max requests
+    windowMs: 60 * 60 * 1000, // 1 Hour
+    message: 'Too many requests' // message to send
+});
+app.use("/players", limit);
+app.use("/players", playersRouter);
 app.listen(port, function() {
     console.log("Runnning on " + port);
 });
